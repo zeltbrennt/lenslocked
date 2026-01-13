9537be3416437fe4b326686703d46338260e7589
Konrad Klawonn <k.klawonn@mailbox.org>
2026-01-13 16:40:27 +0100

    Create SessionID

---
+++ rand/rand.go
@@ -0,0 +1,31 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+func generateBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	nRead, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("bytes: %w", err)
+	}
+	if nRead < n {
+		return nil, fmt.Errorf("bytes: didn't read enough bytes")
+	}
+	return b, nil
+}
+
+func generateString(n int) (string, error) {
+	b, err := generateBytes(n)
+	if err != nil {
+		return "", fmt.Errorf("string: %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func SessionToken(n int) (string, error) {
+	return generateString(n)
+}
