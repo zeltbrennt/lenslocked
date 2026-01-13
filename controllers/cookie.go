6814c81978072f7519244602d0badd8064d3d764
Konrad Klawonn <k.klawonn@mailbox.org>
2026-01-13 16:40:55 +0100

    Add Cookie

---
+++ controllers/cookie.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const cookieSession = "session"
+
+func newCookie(name, value string) *http.Cookie {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		HttpOnly: true,
+		SameSite: http.SameSiteDefaultMode,
+	}
+	return &cookie
+}
+
+func setCookie(w http.ResponseWriter, name, value string) {
+	cookie := newCookie(name, value)
+	http.SetCookie(w, cookie)
+}
+
+func readCookie(r *http.Request, name string) (string, error) {
+	cookie, err := r.Cookie(name)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", name, err)
+	}
+	return cookie.Value, nil
+}
