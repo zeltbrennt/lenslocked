1088083fc599fbaf1a8a5d2b864c2a8ecdc3e097
Konrad Klawonn <k.klawonn@mailbox.org>
2026-01-18 16:40:39 +0100

    Refactor

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 func main() {
-	// setup controllers
+	// setup DB
 	cfg := models.DefaultPostgresConfig()
 	db, err := models.Open(cfg)
 	if err != nil {
@@ -23,12 +23,12 @@ func main() {
 	}
 	defer db.Close()
 
-	// db migrations
 	err = models.MigrateFS(db, migrations.FS, ".")
 	if err != nil {
 		panic(err)
 	}
 
+	// setup services
 	userService := models.UserService{
 		DB: db,
 	}
@@ -36,22 +36,24 @@ func main() {
 		DB: db,
 		TM: models.TokenManager{BytesPerToken: 32},
 	}
-	var userController controllers.Users
-	userController.Templates.Signup = views.Must(views.ParseFS(templates.FS, "signup.html", "layout.html"))
-	userController.Templates.Signin = views.Must(views.ParseFS(templates.FS, "signin.html", "layout.html"))
-	userController.Templates.CurrentUser = views.Must(views.ParseFS(templates.FS, "currentUser.html", "layout.html"))
-	userController.UserService = &userService
-	userController.SessionService = &sessionService
-	// TODO: implement a test to make sure, all Services and Templates are set
 
-	// CSRF
+	// setup middleware
 	protection := http.NewCrossOriginProtection()
 	protection.AddTrustedOrigin("http://localhost:5173")
-	// context
-	//
 	umw := mw.UserMiddleware{
 		SessionService: &sessionService,
 	}
+
+	// setup controllers
+	userController := controllers.Users{
+		UserService:    &userService,
+		SessionService: &sessionService,
+	}
+	userController.Templates.Signup = views.Must(views.ParseFS(templates.FS, "signup.html", "layout.html"))
+	userController.Templates.Signin = views.Must(views.ParseFS(templates.FS, "signin.html", "layout.html"))
+	userController.Templates.CurrentUser = views.Must(views.ParseFS(templates.FS, "currentUser.html", "layout.html"))
+	// TODO: implement a test to make sure, all Services and Templates are set
+
 	// setup router
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -59,17 +61,23 @@ func main() {
 	r.Use(mw.SetHeaders)
 	r.Use(umw.SetUser)
 
+	// setup routes
+	// static
 	r.Get("/", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "home.html", "layout.html"))))
 	r.Get("/contact", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "contact.html", "layout.html"))))
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.html", "layout.html"))))
+	// dynamic
 	r.Get("/signin", userController.SigninPage)
 	r.Post("/signin", userController.HandleSignin)
 	r.Get("/signup", userController.SignupPage)
 	r.Post("/signup", userController.HandleSignup)
-	r.Get("/user/me", userController.CurrentUser)
+	r.Route("/user/me", func(r chi.Router) {
+		r.Use(umw.RequireUser)
+		r.Get("/", userController.CurrentUser)
+	})
 	r.Post("/signout", userController.HandleSignOut)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
